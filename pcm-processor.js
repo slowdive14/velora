d9549c90b6fed714f275c5e9164fa7987f596131
slowdive14 <100013214+slowdive14@users.noreply.github.com>
2026-02-23 13:38:24 +0000

    Deploying to gh-pages from @ slowdive14/velora@7ece90c61cc0bb482649b5d6bebdbc8cb83744dc 🚀

--- pcm-processor.js
+++ pcm-processor.js
@@ -1,12 +1,12 @@
 class PCMProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
-        // Reduced buffer for lower latency (was 4096 = ~256ms at 16kHz)
-        // 2048 samples = ~128ms at 16kHz - good balance between latency and stability
+        // Buffer size: 2048 samples at 16kHz = 128ms
+        // This is proven stable - don't change without thorough testing
         this.bufferSize = 2048;
         this.buffer = new Float32Array(this.bufferSize);
         this.index = 0;
-        this.sampleRate = 16000; // Target sample rate
+        this.sampleRate = 16000;
     }
 
     process(inputs, outputs, parameters) {
@@ -14,26 +14,21 @@ class PCMProcessor extends AudioWorkletProcessor {
         if (input && input.length > 0) {
             const channel = input[0];
 
-            // Check if we need to downsample
-            // Note: globalThis.sampleRate is the context's sample rate
             if (globalThis.sampleRate !== this.sampleRate) {
-                // Downsample input chunk
                 const downsampled = this.downsample(channel, globalThis.sampleRate, this.sampleRate);
 
-                // Fill buffer with downsampled data
                 for (let i = 0; i < downsampled.length; i++) {
                     this.buffer[this.index++] = downsampled[i];
                     if (this.index >= this.bufferSize) {
-                        this.port.postMessage(this.buffer);
+                        this.port.postMessage(this.buffer.slice());
                         this.index = 0;
                     }
                 }
             } else {
-                // No downsampling needed
                 for (let i = 0; i < channel.length; i++) {
                     this.buffer[this.index++] = channel[i];
                     if (this.index >= this.bufferSize) {
-                        this.port.postMessage(this.buffer);
+                        this.port.postMessage(this.buffer.slice());
                         this.index = 0;
                     }
                 }
@@ -42,7 +37,6 @@ class PCMProcessor extends AudioWorkletProcessor {
         return true;
     }
 
-    // Linear Interpolation Downsampling
     downsample(input, sourceRate, targetRate) {
         if (sourceRate === targetRate) return input;
 
@@ -56,9 +50,7 @@ class PCMProcessor extends AudioWorkletProcessor {
             const index2 = Math.min(index1 + 1, input.length - 1);
             const fraction = originalIndex - index1;
 
-            const value1 = input[index1];
-            const value2 = input[index2];
-            result[i] = value1 + (value2 - value1) * fraction;
+            result[i] = input[index1] + (input[index2] - input[index1]) * fraction;
         }
         return result;
     }
